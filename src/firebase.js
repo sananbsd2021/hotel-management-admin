074bf97412c43b7c9516051fb8f9dedfa322e401
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-10-01 10:12:48 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database"
               
 const firebaseConfig = {
-  apiKey: "AIzaSyDzha_p5FDDTlfpUy2x59R_1uHUP2eILtw",
-  authDomain: "jawanpakhackathon.firebaseapp.com",
-  projectId: "jawanpakhackathon",
-  storageBucket: "jawanpakhackathon.appspot.com",
-  messagingSenderId: "845328112334",
-  appId: "1:845328112334:web:80cb6396f82ad915ea4e9f"
+  apiKey: "AIzaSyCuOlM5bsacf3A8KSWA_Qb6DexsOpNJ7jM",
+  authDomain: "manager-app-project-9999b.firebaseapp.com",
+  databaseURL: "https://manager-app-project-9999b-default-rtdb.firebaseio.com",
+  projectId: "manager-app-project-9999b",
+  storageBucket: "manager-app-project-9999b.appspot.com",
+  messagingSenderId: "547133886361",
+  appId: "1:547133886361:web:f7f3eb7508249f90434d08",
+  measurementId: "G-FJRZMKQX4T"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
